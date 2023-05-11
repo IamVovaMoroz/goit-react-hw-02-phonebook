352cd83df673bdbfd77af9b2a53e0c802f112898
IamVovaMoroz <113520901+IamVovaMoroz@users.noreply.github.com>
2023-05-11 14:22:51 +0200

    ok

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Button from '@mui/material/Button';
-// import cssModlue from './ContactList.module.css'
+// import {ContactListContainer} from "./ContactList.styled"
+import './ContactList.css';
 
 export default function ContactList({ contacts, deleteContacts }) {
     const defaultText = 'Not have a contacts';
@@ -8,9 +9,10 @@ export default function ContactList({ contacts, deleteContacts }) {
     // Если contacts существует, отображаем список контактов, нет => отображаем текст по умолчанию
     return contacts ? (
       <ul className="contact-list">
-        {contacts.map((contact) => {
+        {contacts.map((contact, index) => {
           return (
             <li className="contact-item" key={contact.id}>
+               <p className="number-contact">{`${index + 1})`}</p>
               <p className="contact-name">{contact.name}</p>
               <p className="contact-number">{contact.number}</p>
               <Button onClick={() => deleteContacts(contact.id)} variant="contained">
